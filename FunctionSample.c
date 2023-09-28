411af592761482bafbd2829cf7352c992169ae81
Jownieeeey09 <jownieann.malate@dbtc-cebu.edu.ph>
2023-09-28 09:55:08 +0800

    Initial commit

---
+++ FunctionSample.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void printHelloWorld();
+
+void main()
+{
+    printHelloWorld();
+}
+
+
+void printHelloWorld()
+{
+    printf("Hello World");
+    getch();
+}
